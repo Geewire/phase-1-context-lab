9b8a71be15acf5ab9868c6b91ed62e065a6f1f38
root <root@DESKTOP-6I3RBRC>
2023-06-28 13:11:34 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,14 +1,82 @@
 /* Your Code Here */
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+function createEmployeeRecord(employeeInfo) {
+    return {
+       firstName: employeeInfo[0],
+       familyName: employeeInfo[1],
+       title: employeeInfo[2],
+       payPerHour: employeeInfo[3],
+       timeInEvents: [],
+       timeOutEvents: [],
+       
+     }
+   }
+   
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createEmployeeRecords(employeeInfo){
+       return employeeInfo.map(createEmployeeRecord)
+       
+}
+   
 
+function createTimeInEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+  this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date,
+  })
+
+  return this
+}  
+
+
+function createTimeOutEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+  
+    return this
+
+}
+   
+function hoursWorkedOnDate (workDate){
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === workDate
+    })
+  
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === workDate
+    })
+  
+    return (outEvent.hour - inEvent.hour) / 100
+  }
+
+   
+function wagesEarnedOnDate(workdate){
+       const hours = hoursWorkedOnDate.call(this, workdate)
+       this.payPerHour
+       return parseFloat(hours.toString())
+  }
+   
+let findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(function(rec){
+      return rec.firstName === firstName
+    })
+    }
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+        return arrayOfEmployeeRecords.reduce(function(memo, rec){
+            return memo + allWagesFor.call(rec)
+        }, 0)
+      }
+
+//this was given in the lab
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
